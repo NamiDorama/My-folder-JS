92626a2021177c2ee164320b7c22c1403fa0a6d0
NamiDorama <yulliavoitiuk@gmail.com>
2018-02-27 01:39:22 +0200

    HW7 Ready homework

--- Node.js/homeworks/HW7-23.02/js/main.js
+++ Node.js/homeworks/HW7-23.02/js/main.js
@@ -1,2 +1,15 @@
-console.log(process.platform, process.arch);
-console.log(process.cwd());
+const date = new Date();
+const opt = {
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+	hour: 'numeric',
+	minute: 'numeric'
+};
+const path = require('path');
+
+console.log(`Date: ${date.toLocaleDateString("ru", opt)}
+File: ${path.basename(__filename)}`);
+
+const randomNum = require('./tools');
+randomNum(3, 67);
---
+++ Node.js/homeworks/HW7-23.02/js/tools.js
@@ -0,0 +1,3 @@
+const randomNum = (min, max) => console.log( (Math.random() * (max - min) + min).toFixed() );
+
+module.exports = randomNum;
