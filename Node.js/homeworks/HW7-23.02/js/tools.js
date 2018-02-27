f7a23051cfd19c426d48ea95a6a682ea023da186
NamiDorama <yulliavoitiuk@gmail.com>
2018-02-27 11:17:52 +0200

    HW7 improved hometask

--- Node.js/homeworks/HW7-23.02/js/tools.js
+++ Node.js/homeworks/HW7-23.02/js/tools.js
@@ -1,3 +1,3 @@
-const randomNum = (min, max) => console.log( (Math.random() * (max - min) + min).toFixed() );
+const randomNum = (min, max) => console.log( Math.round(Math.random() * (max - min) + min) );
 
 module.exports = randomNum;
