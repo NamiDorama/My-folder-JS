3c983a5123f40e80501a06fad65692497b18c7e8
NamiDorama <yulliavoitiuk@gmail.com>
2018-03-07 11:17:51 +0200

    LS10 add node modules to gitignore

--- Node.js/lessons/LS9_first_server/webpack/webpack.config.js
+++ Node.js/lessons/LS9_first_server/webpack/webpack.config.js
@@ -1,9 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-	entry: {
-		main: './index.js',
-		libs: './lib.js'
-	},
+	entry: './app.js',
+	context: path.resolve('src'),
 	output: {
-		filename: 'bundle.js'
+		filename: 'bundle-[name].js',
+		path: path.resolve('public')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,  //  что нe должно быть пропущено через загрузчик
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Mega project', // --> имеет меньший приоритет, если указываем template
+			template: 'index.html'
+			// filename: 'name.html', --> имя выходящего файта
+			// favicon: 'images/favicon.ico' --> откуда брать иконку
+		})
+	],
+
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		},
 	}
+
 };
